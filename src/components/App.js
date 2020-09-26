7e9bc8909e7daa81f3ec78a5775ba4025b69b95b
Tham53 <texas01988@gmail.com>
2020-09-25 22:50:39 -0500

    new

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'
+import Memeapi from './Memeapi'
+function App (){
+    return(
+        <div>
+            <Header/>
+            <Memeapi/>
+
+        </div>
+    )
+}
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Header(){
+    return(
+        <header>
+            <img 
+               src="http://www.pngall.com/wp-content/uploads/2016/06/Forever-Alone-Transparent.png" 
+               alt="Problem?"
+            />
+            <p>Tommys Meme Creator</p>
+        </header>
+    )
+
+}
+export default Header
